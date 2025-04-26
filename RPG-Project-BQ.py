9d5cf2c1367f7a98c81034c552b528fbcbd69c5e
Fardan Bin Khalid <itrunsidkhow@gmail.com>
2025-04-27 00:43:01 +0500

    Updated Nouns RPG-Project-BQ.py

--- RPG-Project-BQ.py
+++ RPG-Project-BQ.py
@@ -1,4 +1,4 @@
-print("\n'Beast Of The Indus' - A Text Based RPG by Fardan, Isharb and Shabeeb.\n")
+print("\n'Beast Of The Indus' - A Text Based RPG by Fardan, Isharab and Shabeeb.\n")
 
 #player's bio data
 
